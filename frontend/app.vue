0a8d9ed01529e2643eb24022aa12025374e8629c
ChenLin-Wang <wangchenlin2001@icloud.com>
2025-03-02 22:25:50 +0800

    V0.1.2.3

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <v-app>
+        <v-btn class="bg-black" @click="toggleTheme">
+            Toogle theme
+        </v-btn>
+        <div class="bg-primary text-center" style="height: 100px;">
+            <p class="my-10">Primary</p>
+        </div>
+        <div class="bg-secondary text-center" style="height: 100px;">
+            <p class="my-10">Secondary</p>
+        </div>
+        <div class="bg-darken1 text-center" style="height: 100px;">
+            <p class="my-10">Darken1</p>
+        </div>
+        <div class="bg-darken2 text-center" style="height: 100px;">
+            <p class="my-10">Darken2</p>
+        </div>
+    </v-app>
 </template>
+
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme() {
+    theme.global.name.value = theme.global.current.value.dark ? 'lightTheme' : 'darkTheme'
+}
+</script>
--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -5,8 +5,30 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 export default defineNuxtPlugin((app) => {
-  const vuetify = createVuetify({
-    // ... your configuration
-  })
-  app.vueApp.use(vuetify)
+    const vuetify = createVuetify({
+        theme: {
+            defaultTheme: 'lightTheme',
+            themes: {
+                lightTheme: {
+                    dark: false,
+                    colors: {
+                        primary: '#A1E3F9',
+                        secondary: '#D1F8EF',
+                        darken1: "#578FCA",
+                        darken2: "#3674B5"
+                    }
+                },
+                darkTheme: {
+                    dark: true,
+                    colors: {
+                        primary: '#A5C9CA',
+                        secondary: '#E7F6F2',
+                        darken1: "#395B64",
+                        darken2: "#2C3333"
+                    }
+                }
+            }
+        }
+    })
+    app.vueApp.use(vuetify)
 })
